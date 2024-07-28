ac60058b7bd59888bcf67fb0b9fea666413d5690
eebette <eric.bette@pm.me>
2024-07-28 10:49:08 +0900

    Bumping delay in search for otp

--- bank_scrapers/common/functions.py
+++ bank_scrapers/common/functions.py
@@ -151,7 +151,7 @@ def search_files_for_int(
     timeout: int,
     file_ext: str = ".txt",
     reverse: bool = False,
-    delay=15,
+    delay: int = 30,
 ) -> str:
     """
     Searches files in a directory for an integer of specific length, and returns the integer
