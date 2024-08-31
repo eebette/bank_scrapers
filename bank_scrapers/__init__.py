8476ffee19b3fc8bccc7d940b55ffa690a50fe88
eebette <eric.bette@pm.me>
2024-08-31 14:22:15 +0900

    Vanguard fix for holiday closures notification

--- bank_scrapers/__init__.py
+++ bank_scrapers/__init__.py
@@ -5,7 +5,7 @@ Provides library level metadata and constants.
 import os
 
 NAME: str = "bank_scrapers"
-VERSION: str = "1.2.17"
+VERSION: str = "1.2.18"
 ROOT_DIR: str = os.path.dirname(os.path.abspath(__file__))
 
 
