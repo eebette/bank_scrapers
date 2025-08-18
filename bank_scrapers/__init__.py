f3bf108f543093ff2fb9f9e1a0829cdb59fa7392
eebette <eric.bette@pm.me>
2025-08-18 15:54:22 +0900

    Version bump for various bug fixes

--- bank_scrapers/__init__.py
+++ bank_scrapers/__init__.py
@@ -5,7 +5,7 @@ Provides library level metadata and constants.
 import os
 
 NAME: str = "bank_scrapers"
-VERSION: str = "1.4.1"
+VERSION: str = "1.4.2"
 ROOT_DIR: str = os.path.dirname(os.path.abspath(__file__))
 
 
