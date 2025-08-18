8f8b05f674ea938fe310386c051a9e82da1b6c20
eebette <eric.bette@pm.me>
2025-08-18 14:38:40 +0900

    Patchright migration for Vanguard

--- bank_scrapers/scrapers/vanguard/driver.py
+++ bank_scrapers/scrapers/vanguard/driver.py
@@ -20,13 +20,13 @@ from random import randint, uniform
 
 # Non-Standard Imports
 import pandas as pd
-from undetected_playwright.async_api import (
+from patchright.async_api import (
     async_playwright,
     Playwright,
     Page,
     Locator,
     expect,
-    Browser,
+    BrowserContext,
     Download,
 )
 from pyvirtualdisplay import Display
@@ -419,10 +419,11 @@ async def run(
     :return: A list of pandas dataframes of accounts info tables
     """
     # Instantiate browser
-    browser: Browser = await playwright.chromium.launch(
+    browser: BrowserContext = await playwright.chromium.launch_persistent_context(
+        user_data_dir=str(),
         channel="chrome",
         headless=False,
-        args=["--disable-blink-features=AutomationControlled"],
+        no_viewport=True,
     )
     page: Page = await browser.new_page()
 
